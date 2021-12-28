0278712d59f3086ccc74959e7c3a5c32bc60b087
Ren xiaofei <Greenarrow961224@outlook.com>
2021-12-28 15:05:01 +0800

    init project

---
+++ src/main/java/com/arrow/mp/ArrowWxMpApplication.java
@@ -0,0 +1,18 @@
+package com.arrow.mp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Greenarrow
+ * @date 2021-12-28 11:30
+ * 项目启动类
+ */
+@SpringBootApplication
+public class ArrowWxMpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArrowWxMpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arrow/mp/ArrowWxMpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arrow.mp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArrowWxMpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
